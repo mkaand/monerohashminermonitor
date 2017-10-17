d3e799fd1a4d00d29a65abdf802045b4a74463e5
Kaan Dogan <mkaand@gmail.com>
2017-10-18 01:45:06 +0300

    Update monerohash_rss.php

--- monerohash_rss.php
+++ monerohash_rss.php
@@ -82,9 +82,9 @@ function str_replace_first($from, $to, $subject)
 }
 
 If ($status == $online_offline){
-$guid = date("Y-m-d-h");	
+$guid = date("Y-m-d-H");	
 }else{
-$guid = date("Y-m-d-h") . mt_rand();	
+$guid = date("Y-m-d-H") . mt_rand();	
 If ($online_offline=="Miner is offline"){file_put_contents($myfile, str_replace_first('$online_offline="Miner is offline"; //DO NOT CHANGE','$online_offline="Miner is online"; //DO NOT CHANGE', $file_content));}
 If ($online_offline=="Miner is online"){file_put_contents($myfile, str_replace_first('$online_offline="Miner is online"; //DO NOT CHANGE','$online_offline="Miner is offline"; //DO NOT CHANGE', $file_content));}
 }
